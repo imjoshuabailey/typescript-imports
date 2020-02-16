8738ca910810f80fd4c6de75558919a506439e4a
Josh Bailey <joshuaclaytonbailey@gmail.com>
2020-02-16 00:42:37 -0800

    exported a person object and function

---
+++ challenge-three/index.js
@@ -0,0 +1,6 @@
+"use strict";
+exports.__esModule = true;
+var person_1 = require("./person");
+var person_2 = require("./person");
+exports.salutations = person_2.greet(person_1.person.firstName);
+console.log(person_1.person);
---
+++ challenge-three/index.ts
@@ -0,0 +1,6 @@
+import {person} from './person'
+import {greet} from './person'
+
+export const salutations = greet(person.firstName)
+console.log(person)
+
