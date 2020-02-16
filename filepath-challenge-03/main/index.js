5eb5a4d28e4fa5e27db70a0019aacefaea122afd
Josh Bailey <joshuaclaytonbailey@gmail.com>
2020-02-16 00:53:25 -0800

    imported function into nested file from a nested file

--- filepath-challenge-03/main/index.js
+++ filepath-challenge-03/main/index.js
@@ -1,7 +1,8 @@
+"use strict";
+exports.__esModule = true;
 /* import the double function from double.ts
    now invoke the function with 1024 as an input. Store the output in an exported variable named result.
    confirm completion by running tests for this directory.
 */
-import {double} from '../js/functions/math/double'
-
-export const result = double(1024);
+var double_1 = require("../js/functions/math/double");
+exports.result = double_1.double(1024);
